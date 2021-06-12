c2b28d1964e7380eec63ba2c676a317777d5e489
Stefano Unlayao <stefanounlayao@hotmail.ca>
2021-06-12 18:30:55 -0400

    Added disabled bottom bar

--- lib/providers/main_providers.dart
+++ lib/providers/main_providers.dart
@@ -4,3 +4,5 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 final panelControllerProvider = StateProvider<PanelController>((ProviderReference ref) => PanelController());
+
+final isAddingNewCoordinate = StateProvider<bool>((ProviderReference ref) => false);
