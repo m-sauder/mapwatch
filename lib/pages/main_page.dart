193cf0b0f74819ef3a5fd29232006a80e8608794
stefano-u <stefanounlayao@Hotmail.ca>
2021-06-12 01:48:35 -0400

    Added Slide Bar and Top Right Button (#4)
    * Added slide bar up
    
    * Added stack

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MainPageWidget extends StatefulWidget {
   @override
@@ -11,24 +12,61 @@ class _HomePageState extends State<MainPageWidget> {
   //UI of the Main Page
   @override
   Widget build(BuildContext context) {
-    return new FlutterMap(
-      options: new MapOptions(
-        center: new LatLng(44.17, -81.64),
-        zoom: 13.0,
-      ),
-      layers: [
-        new TileLayerOptions(urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", subdomains: ['a', 'b', 'c']),
-        new MarkerLayerOptions(
-          markers: [
-            new Marker(
-              width: 80.0,
-              height: 80.0,
-              point: new LatLng(51.5, -0.09),
-              builder: (ctx) => new Container(),
+    return Scaffold(
+      body: SlidingUpPanel(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24.0),
+          topRight: Radius.circular(24.0),
+        ),
+        backdropEnabled: true,
+        maxHeight: MediaQuery.of(context).size.height - 80,
+        minHeight: 30,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        panel: Center(
+          child: Text('hey asshole'),
+        ),
+        body: Stack(
+          children: [
+            FlutterMap(
+              options: MapOptions(
+                center: LatLng(44.17, -81.64),
+                zoom: 13.0,
+              ),
+              layers: [
+                TileLayerOptions(urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", subdomains: ['a', 'b', 'c']),
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(51.5, -0.09),
+                      builder: (ctx) => Container(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: EdgeInsets.only(top: 40, right: 10),
+                child: Container(
+                  width: 25,
+                  height: 25,
+                  child: FloatingActionButton(
+                    child: Icon(
+                      Icons.gps_fixed,
+                      size: 18,
+                    ),
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
