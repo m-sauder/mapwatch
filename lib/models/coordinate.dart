bb76694c0f53a0f0c62ea07bbd7e409720c57151
Jeffrey Jaca <jeffreyjjaca@gmail.com>
2021-06-12 17:29:14 -0400

    Firestore models (#11)
    * test
    
    * Added build.gradle changes
    
    * Added initial flutter loader
    
    * added revised google-services.json
    
    * Added remaining firestore dependencies
    
    * added model classes
    
    * added read/write functions to db for coordinates
    
    * added imports main_page.dart
    
    * removed unused imports
    
    Co-authored-by: Stefano Unlayao <stefanounlayao@hotmail.ca>

---
+++ lib/models/coordinate.dart
@@ -0,0 +1,36 @@
+import 'dart:typed_data';
+
+class Coordinate {
+  String id;
+  String title;
+  String description;
+  double latitude;
+  double longitude;
+  Uint8List image;
+
+  Coordinate(
+    this.id,
+    this.title,
+    this.description,
+    this.latitude,
+    this.longitude,
+    this.image,
+  );
+
+  Coordinate.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        description = json['description'],
+        latitude = json['latitude'],
+        longitude = json['longitude'],
+        image = json['image'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'description': description,
+        'latitude': latitude,
+        'longitude': longitude,
+        'image': image,
+      };
+}
